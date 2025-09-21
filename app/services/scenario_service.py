8152c7e8abaae464d048bc9a33a8ad8a3d7ed4eb
Javad <you@example.com>
2025-09-21 04:44:00 +0330

    embed search v2

--- app/services/scenario_service.py
+++ app/services/scenario_service.py
@@ -70,7 +70,7 @@ async def check_scenario_one(request: ChatRequest, db: AsyncSession, http_reques
             scenario, product_name = await classify_scenario_for_embed(request)
             logger.info(f"CLASSIFIED SCENARIO: {scenario}, product_name: {product_name}")
             if scenario in ["SCENARIO_1_DIRECT_SEARCH", "SCENARIO_2_FEATURE_EXTRACTION", "SCENARIO_3_SELLER_INFO"]:
-                found_key = await find_exact_product_name_service_and_embed(user_message = request.messages[-1].content.strip(), product_name=product_name)
+                found_key = await find_exact_product_name_service_and_embed(user_message = request.messages[-1].content.strip(), possible_product_name=product_name)
                 if not found_key and scenario in ["SCENARIO_1_DIRECT_SEARCH", "SCENARIO_2_FEATURE_EXTRACTION", "SCENARIO_3_SELLER_INFO"]:
                     raise HTTPException(status_code=404, detail="No products found matching the keywords.")
             
